87020ad1c1b3131fbc467c061500bb506faec0fb
Boisxon <boisxon357@gmail.com>
2024-12-05 19:26:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+let ism = prompt("ismingizni kiriting")
+console.log(ism);
+
+let familia = prompt("familangizni kiriting")
+console.log(familia);
+
+let yosh = +prompt("yoshingizni kiriting")
+console.log(yosh);
+
